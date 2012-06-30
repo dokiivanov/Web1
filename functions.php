40fcc51f9d33a7e4e0dd1077e01b6eb331e1c717
Andon Ivanov <Virosss@abv.bg>
2012-06-30 07:46:48 +0300

    Update master

--- functions.php
+++ functions.php
@@ -8,8 +8,5 @@ register_sidebar(array(
 	'before_title' => '<h5 class="widgettitle">',
 	'after_title' => '</h5>',
 ));
-function dateses(){
-	echo 'test';
-}
 
 add_theme_support('automatic-feed-links');
